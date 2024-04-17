f93605b1a9143cf99188272b14fcbaa3a6e40b46
HanusLubos <lubos.hanus6@gmail.com>
2024-04-17 08:39:56 +0200

    read_data

--- sorting.py
+++ sorting.py
@@ -1,3 +1,4 @@
+import csv
 import os
 
 
@@ -10,9 +11,22 @@ def read_data(file_name):
     """
     cwd_path = os.getcwd()
     file_path = os.path.join(cwd_path, file_name)
+    with open(file_path, mode="r") as csv_file:
+        reader = csv.DictReader(csv_file)
+        data = {"series_1":[], "series_2":[], "series_3": []}
+
+        for row in reader:
+            for key in row.keys():
+                if key not in data:
+                    data[key] = [row[key]]
+                else:
+                    data[key].append(int(row[key]))
+
+    return data
 
 
 def main():
+    read_data("numbers.csv")
     pass
 
 
